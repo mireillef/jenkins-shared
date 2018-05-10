32430a4e5965966f7b4f498248376cc969c88d30
root <mireillef@directeam.com>
2018-05-10 15:49:21 +0300

    create slack messagewq

--- vars/DockerDeploy.groovy
+++ vars/DockerDeploy.groovy
@@ -25,6 +25,10 @@ node {
   try {
             sh "wget http://localhost:8888/directeam -O /dev/null"
             return true
+            def NotifySuccess() {
+  slackSend (color: '#FFFF00', message: "Build: ${env.JOB_NAME} Number: [${env.BUILD_NUMBER}] has succeeded.")
+}
+	    NotifySuccess()
         } catch (Exception e) {
             throw e;
     }
